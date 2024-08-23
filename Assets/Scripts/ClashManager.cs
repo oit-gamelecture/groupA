9293e8458fdabd8bd6e746ad3943174bc33d82d6
辻響 <0131Hibiki-0131@users.noreply.github.com>
2024-08-23 17:02:15 +0900

    8/23　辻響　17:01　障害物にあたったら衝突音がなる処理の実装。

---
+++ Assets/Scripts/ClashManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClashManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "enemy" )
+        {
+            audioSource.Play();
+           
+        }
+
+    }
+}
